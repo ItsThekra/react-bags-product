ae97bd5a0179097a2d51704300dc14208d1f30ae
Thekra Aljagthmi <thekra.aljagthmi@gmail.com>
2025-05-19 13:07:28 +0300

    Update on items

--- src/App.jsx
+++ src/App.jsx
@@ -12,31 +12,31 @@ export default function App() {
       image: 'https://images.pexels.com/photos/2081199/pexels-photo-2081199.jpeg?auto=compress&cs=tinysrgb&w=400',
       title: 'Card 1',
       description: 'This is bag 1',
-      items: ['Item A', 'Item B', 'Item C']
+      items: ['01', '02', '03']
     },
     {
       image: 'https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg?auto=compress&cs=tinysrgb&w=400',
       title: 'Card 2',
       description: 'This is bag 2',
-      items: ['Item A', 'Item B', 'Item C']
+      items: ['01', '02', '03']
     },
     {
       image: 'https://images.pexels.com/photos/3731256/pexels-photo-3731256.jpeg?auto=compress&cs=tinysrgb&w=400',
       title: 'Card 3',
       description: 'This is bag 3',
-      items: ['Item A', 'Item B', 'Item C']
+      items: ['01', '02', '03']
     },
     {
       image: 'https://images.pexels.com/photos/2977304/pexels-photo-2977304.jpeg?auto=compress&cs=tinysrgb&w=400',
       title: 'Card 4',
       description: 'This is bag 4',
-      items: ['Item A', 'Item B', 'Item C']
+      items: ['01', '02', '03']
     },
     {
       image: 'https://images.pexels.com/photos/1986996/pexels-photo-1986996.jpeg?auto=compress&cs=tinysrgb&w=400',
       title: 'Card 5',
       description: 'This is bag 5',
-      items: ['Item A', 'Item B', 'Item C']
+      items: ['01', '02', '03']
     }
   ];
 
