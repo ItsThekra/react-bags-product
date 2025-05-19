c7ddac113ac32842ca7da9ee8a793ed37fccdb35
Thekra Aljagthmi <thekra.aljagthmi@gmail.com>
2025-05-19 13:49:39 +0300

    Update on the items

--- src/App.jsx
+++ src/App.jsx
@@ -12,31 +12,31 @@ export default function App() {
       image: 'https://images.pexels.com/photos/2081199/pexels-photo-2081199.jpeg?auto=compress&cs=tinysrgb&w=400',
       title: 'Bag - 1',
       description: 'This is bag 1',
-      items: ['01', '02', '03']
+      items: ['green', 'small bag', '20$']
     },
     {
       image: 'https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg?auto=compress&cs=tinysrgb&w=400',
       title: 'Bag - 2',
       description: 'This is bag 2',
-      items: ['01', '02', '03']
+      items: ['Orange', 'small bag', '15$']
     },
     {
       image: 'https://images.pexels.com/photos/3731256/pexels-photo-3731256.jpeg?auto=compress&cs=tinysrgb&w=400',
       title: 'Bag - 3',
       description: 'This is bag 3',
-      items: ['01', '02', '03']
+      items: ['Black', 'Big bag', '20$']
     },
     {
       image: 'https://images.pexels.com/photos/2977304/pexels-photo-2977304.jpeg?auto=compress&cs=tinysrgb&w=400',
       title: 'Bag - 4',
       description: 'This is bag 4',
-      items: ['01', '02', '03']
+      items: ['Orange', 'medium bag', '15$']
     },
     {
       image: 'https://images.pexels.com/photos/1986996/pexels-photo-1986996.jpeg?auto=compress&cs=tinysrgb&w=400',
       title: 'Bag - 5 ',
       description: 'This is bag 5',
-      items: ['01', '02', '03']
+      items: ['Red', 'Big bag', '30$']
     }
   ];
 
