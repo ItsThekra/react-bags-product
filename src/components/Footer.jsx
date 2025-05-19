0b93392304908cfe88f50550b9f8419669853777
Thekra Aljagthmi <thekra.aljagthmi@gmail.com>
2025-05-19 13:04:58 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer 
+    style={{ padding: '10px',
+             borderTop: '1px solid #ccc', 
+             marginTop: '20px' }}>
+
+          <p>&copy; 2025 - All rights reserved for the store</p>
+    </footer>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav 
+    style={{
+        padding: '15px',
+        borderBottom: '1px solid #ccc'
+    }}>
+        <h2>Bag's Store</h2>
+    </nav>
+  )
+}
